d44577e780a6e9ceecad9629996bbbc4751b6759
Alec Kim <kimalec7@gmail.com>
2014-06-20 18:33:47 +0900

    기본 Test unit 추가.

---
+++ models/init.js
@@ -0,0 +1,10 @@
+/**
+ * Created by aleckim on 2014. 6. 20..
+ */
+
+exports.getTasks = function() {
+    return {
+        title: ["Hello Wizard"],
+        user: ["respond with a resource"]
+    };
+};
