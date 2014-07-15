5eefb73dfac0186fe4b8ab3023836eac0013a18b
Alec Kim <kimalec7@gmail.com>
2014-07-16 05:37:37 +0900

    JUS-19
    add userdb.js, facebook.js, tumblr.js, test_userdb.js
    remove test.js
    up core.js - Getting session was changed to user
    up index.html - add sign stat
    up signin.html - ng-click event to href
    up app.js - add session to use
    up package.json - add express-session, passport series
     remove wordpressrest

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+
+
+var userdb = require('./models/userdb');
 
 var wordpress = require('./routes/wordpress');
+var facebook = require('./routes/facebook');
+var tumblr = require('./routes/tumblr');
 
 var app = express();
 
@@ -15,8 +22,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/api/wordpress', wordpress);
+app.use('/facebook', facebook);
+app.use('/wordpress', wordpress);
+app.use('/tumblr', tumblr);
+
+app.use('/user', function (req, res) {
+   if (!req.user) {
+        res.write('NAU');
+   }
+   else {
+       res.write(req.user.providers[0].displayName);
+   }
+   res.end();
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/userdb.js
@@ -0,0 +1,188 @@
+/**
+ * Created by aleckim on 2014. 7. 11..
+ */
+
+var fs = require('fs');
+var dbfilename = 'users.db';
+
+
+function userdb() {
+
+}
+
+userdb.users = [];
+/*
+   [
+        {
+            "id":1,
+            "providers": [
+                {"providerName":"facebook", "accessToken":accessToken,"providerId":profile.id },
+                {"providerName":"twitter", "data":"dummy2"}
+            ]
+        }
+    ];
+*/
+
+userdb.init = function () {
+  try {
+    userdb.users = JSON.parse(fs.readFileSync(dbfilename)).user_db;
+  }
+  catch (e) {
+    console.log(e);
+    return false;
+  }
+
+  return true;
+};
+
+userdb.saveFile = function () {
+  try {
+    fs.writeFile(dbfilename, JSON.stringify({"user_db":userdb.users}), function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+  }
+  catch(e) {
+      console.log(e);
+      return false;
+  }
+
+  return true;
+};
+
+userdb.getUserCount = function () {
+    return this.users.length;
+};
+
+userdb.getProviderCount = function (id) {
+    var len = 0;
+    for (var i=0; i<this.users.length; i++) {
+        if (this.users[i].id == id) {
+            len = this.users[i].providers.length;
+            break;
+        }
+    }
+
+    if (len == 0) {
+        console.log("Fail to find id");
+    }
+
+    return len;
+};
+
+userdb.addUser = function (new_provider) {
+    var totalCount = 0;
+    var lastId = 0;
+
+    totalCount = this.getUserCount();
+    //console.log("total = " + totalCount);
+
+    if (totalCount > 0) {
+        lastId = this.users[totalCount - 1].id;
+    }
+    //console.log("lastId = " + lastId);
+    lastId++;
+
+    this.users.push({"id":lastId, "providers":[new_provider]});
+
+    //this.saveFile();
+
+    return this.users[totalCount];
+};
+
+userdb.removeUser = function (id) {
+    for (var i = 0;i<this.getUserCount();i++) {
+        if (this.users[i].id == id) {
+            delete this.users[i];
+            return true;
+        }
+    }
+
+    console.log("Fail to find user");
+    return false;
+};
+
+userdb.addProvider = function (id, new_provider) {
+    var user = this.findUser(id);
+
+    user.providers.push(new_provider);
+
+    //this.saveFile();
+
+    return user;
+};
+
+userdb.removeProvider = function (id, providerName) {
+
+     for (var i=0; i<this.users.length; i++) {
+        if (this.users[i].id == id) {
+            for (var j=0; j<this.users[i].providers.length; j++) {
+                if (this.users[i].providers[j].providerName == providerName) {
+                    delete this.users[i].providers[j];
+                    return true;
+                }
+            }
+
+            console.log("There is id, but fail to find provider by providerName");
+        }
+    }
+
+    console.log("Fail to find provider by providerName");
+
+    return false;
+};
+
+userdb.findUser = function (id) {
+    //console.log("Find user = " + id);
+
+    for (var i=0; i<this.users.length; i++) {
+
+        if (this.users[i].id == id) {
+
+            //console.log(this.users[i]);
+
+            return this.users[i];
+        }
+    }
+
+    console.log("Fail to find user by id");
+    return null;
+};
+
+userdb.findUserByProvider = function (provider) {
+    for (var i=0; i<this.users.length; i++) {
+        for (var j=0; j<this.users[i].providers.length; j++) {
+            if (this.users[i].providers[j].providerName == provider.providerName
+                && this.users[i].providers[j].providerId == provider.providerId) {
+                return this.users[i];
+            }
+        }
+    }
+
+    console.log("Fail to find user by provider");
+    return null;
+};
+
+userdb.findProvider = function (id, providerName) {
+    //console.log("Find id= " + id + " provider= " + provider);
+
+    for (var i=0; i<this.users.length; i++) {
+        if (this.users[i].id == id) {
+            for (var j=0; j<this.users[i].providers.length; j++) {
+                if (this.users[i].providers[j].providerName == providerName) {
+                    return this.users[i].providers[j];
+                }
+            }
+
+            console.log("There is id, but fail to find provider by provider");
+        }
+    }
+
+    console.log("Fail to find provider by providerName");
+
+    return null;
+};
+
+module.exports = userdb;
+
+
