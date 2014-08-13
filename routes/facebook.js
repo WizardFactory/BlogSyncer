ac6aab0a9e8bf67c826e72941591c90fa1fd2021
Alec Kim <kimalec7@gmail.com>
2014-08-13 14:42:17 +0900

    JUS-36 facebook 기본 API 와 상태 정보 가지고 오기.

--- routes/facebook.js
+++ routes/facebook.js
@@ -6,12 +6,14 @@ var userdb = require('../models/userdb');
 
 var express = require('express');
 var passport = require('passport');
+var request = require('request');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 var router = express.Router();
 
 var FACEBOOK_CLIENT_ID = "1447794722165916";
 var FACEBOOK_CLIENT_SECRET = "adf699b010b780c8808b3ebeb755e5ab";
+var API_FACEBOOK_COM = "https://graph.facebook.com";
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -60,5 +62,31 @@ router.get('/authorized',
         res.redirect('/#');
     }
 );
+
+router.get('/me', function (req, res) {
+    if (!req.user) {
+        var errorMsg = 'You have to login first!';
+        console.log(errorMsg);
+        res.send(errorMsg);
+        res.redirect("/#/signin");
+    }
+    else {
+        var p = userdb.findProvider(req.user.id, "facebook");
+
+        var api_url = API_FACEBOOK_COM+"/me";
+
+        console.log(api_url);
+
+        request.get(api_url, {
+                json: true,
+                headers: {
+                    "authorization": "Bearer " + p.accessToken
+                }
+            }, function (err, response, data) {
+                console.log(data);
+                res.send(data);
+        });
+    }
+});
 
 module.exports = router;
