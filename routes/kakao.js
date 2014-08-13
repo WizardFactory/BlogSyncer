7de43d19a8765d60c8b5f0c9574fd43dc3a19a69
Alec Kim <kimalec7@gmail.com>
2014-08-13 14:34:59 +0900

    JUS-35 kakao 기본 정보와 posts 가지고 오는 api 생성.

--- routes/kakao.js
+++ routes/kakao.js
@@ -6,11 +6,13 @@ var userdb = require('../models/userdb');
 
 var express = require('express');
 var passport = require('passport');
+var request = require('request');
 var KakaoStrategy = require('passport-kakao').Strategy;
 
 var router = express.Router();
 
 var KAKAO_CLIENT_ID = "d76e3616d42d5a2507183f717aff6579";
+var API_KAKAO_COM = "https://kapi.kakao.com";
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -58,5 +60,57 @@ router.get('/authorized',
         res.redirect('/#');
     }
 );
+
+router.get('/me', function (req, res) {
+    if (!req.user) {
+        var errorMsg = 'You have to login first!';
+        console.log(errorMsg);
+        res.send(errorMsg);
+        res.redirect("/#/signin");
+    }
+    else {
+        var p = userdb.findProvider(req.user.id, "kakao");
+
+        var api_url = API_KAKAO_COM+"/v1/user/me";
+
+        console.log(api_url);
+
+        request.get(api_url, {
+                json: true,
+                headers: {
+                    "authorization": "Bearer " + p.accessToken
+                }
+            }, function (err, response, data) {
+                console.log(data);
+                res.send(data);
+        });
+    }
+});
+
+router.get('/mystories', function (req, res) {
+    if (!req.user) {
+        var errorMsg = 'You have to login first!';
+        console.log(errorMsg);
+        res.send(errorMsg);
+        res.redirect("/#/signin");
+    }
+    else {
+        var p = userdb.findProvider(req.user.id, "kakao");
+
+        var api_url = API_KAKAO_COM+"/v1/api/story/mystories";
+
+        console.log(api_url);
+
+        request.get(api_url, {
+                json: true,
+                headers: {
+                    "authorization": "Bearer " + p.accessToken
+                }
+            }, function (err, response, data) {
+                console.log(data);
+                res.send(data);
+        });
+    }
+});
 
 module.exports = router;
