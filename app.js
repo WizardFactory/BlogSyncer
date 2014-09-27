482114279d5fa50973502f82b99dddec54bd13fc
Alec Kim <kimalec7@gmail.com>
2014-09-27 16:19:49 +0900

    app.js 없는 페이지 요청시에 정상적으로 에러 던질 수 있게, error jade 추가
    blogbot.js
    error를 수신하는 경우에 예외처리해서 빠져나오게 수정

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ var blogRoutes = require('./routes/blogRoutes');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
