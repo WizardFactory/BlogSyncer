18efa7a9bcc5e2ce117f16ba503a3ad3b732fbb4
Alec Kim <kimalec7@gmail.com>
2014-11-15 21:14:16 +0900

    JUS-72 dbfile에서 mongodb로 변경되면서 unit test 오류 수정. Fixed #96
    *blogdb.js
    ** collection name changes to Site from SiteDb.
    * postdb.js
    ** default type of post changes to 'text'.
    * test_userdb.js
    ** update for monogodb
    * add test_blogdb.js, test_groupdb, test_historydb, test_postdb, test_userdb

---
+++ test/test_historydb.js
@@ -0,0 +1,34 @@
+/**
+ * Created by aleckim on 14. 11. 15..
+ */
+
+var assert  = require('assert');
+var History = require('../models/historydb');
+
+var d = new Date();
+
+var testHistory = {
+    tryTime: d,
+    status: "200",
+    src: {
+        title: "XXXXX", id: "ASDFED", url: "DJDJDJD"
+    },
+    dst: {
+        id: "DFCddd",  url: "e83ed0"
+    }
+};
+
+describe('historyDb', function () {
+    describe('Function', function () {
+        var historyDb;
+        it('create historyDb', function () {
+            historyDb = new History();
+            assert.notEqual(typeof historyDb, "undefined", "Fail to create historyDb");
+        });
+        it('add historyDb', function () {
+            historyDb.histories.push(testHistory);
+            assert.equal(historyDb.histories[0].src.title, testHistory.src.title, "Fail to add historyDb");
+        });
+    });
+});
+
