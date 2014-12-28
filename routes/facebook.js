516032b8727e2a8ceffa398b7f15dc278daf3fc2
dsshin <dsshin@innopiatech.com>
2014-12-28 14:16:21 +0900

    #111  JUS-81 Facebook bot_bloglist API 구현 #119 JUS-86 Facebook me API 수정

--- routes/facebook.js
+++ routes/facebook.js
@@ -20,16 +20,19 @@ var log = require('winston');
 var FACEBOOK_API_URL = "https://graph.facebook.com";
 
 passport.serializeUser(function(user, done) {
+    "use strict";
     done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
+    "use strict";
     done(null, obj);
 });
 
 function _updateOrCreateUser(req, provider, callback) {
-    UserDb.findOne({'providers.providerName':provider.providerName
-            , 'providers.providerId': provider.providerId},
+    "use strict";
+    UserDb.findOne({'providers.providerName':provider.providerName,
+            'providers.providerId': provider.providerId},
         function (err, user) {
             var p;
             var isNewProvider = false;
@@ -106,6 +109,7 @@ passport.use(new FacebookStrategy({
         passReqToCallback : true
     },
     function(req, accessToken, refreshToken, profile, done) {
+        "use strict";
 //        log.debug("accessToken:" + accessToken);
 //        log.debug("refreshToken:" + refreshToken);
 //        log.debug("profile:" + JSON.stringify(profile));
@@ -147,6 +151,7 @@ router.get('/authorize',
 router.get('/authorized',
     passport.authenticate('facebook', { failureRedirect: '/#signin' }),
     function(req, res) {
+        "use strict";
         // Successful authentication, redirect home.
         log.debug('Successful!');
         res.redirect('/#');
@@ -159,6 +164,7 @@ router.get('/authorized',
  * @returns {number}
  */
 getUserId = function (req) {
+    "use strict";
     var userid = 0;
 
     if (req.user) {
@@ -174,6 +180,7 @@ getUserId = function (req) {
 };
 
 function _getUserID(req) {
+    "use strict";
     var userid = 0;
 
     if (req.user) {
@@ -189,6 +196,7 @@ function _getUserID(req) {
 }
 
 function _checkError(err, response, body) {
+    "use strict";
     if (err) {
         log.debug(err);
         return err;
@@ -202,6 +210,7 @@ function _checkError(err, response, body) {
 }
 
 function _requestGet(url, accessToken, callback) {
+    "use strict";
     request.get(url, {
         json: true,
         headers: {
@@ -213,6 +222,7 @@ function _requestGet(url, accessToken, callback) {
 }
 
 router.get('/me', function (req, res) {
+    "use strict";
     var user_id = _getUserID(req);
     if (user_id == 0) {
         var errorMsg = 'You have to login first!';
@@ -222,7 +232,7 @@ router.get('/me', function (req, res) {
         return;
     }
 
-    User.findById(user_id, function (err, user) {
+    UserDb.findById(user_id, function (err, user) {
         var p;
         var api_url;
 
@@ -243,14 +253,13 @@ router.get('/me', function (req, res) {
     });
 });
 
-
 router.get('/bot_bloglist', function (req, res) {
+    "use strict";
 
     log.debug("facebook: "+ req.url + ' : this is called by bot');
 
     var userId = _getUserID(req);
-
-    if (userId == 0) {
+    if (userId === 0) {
         var errorMsg = 'You have to login first!';
         log.debug(errorMsg);
         res.send(errorMsg);
@@ -258,7 +267,7 @@ router.get('/bot_bloglist', function (req, res) {
         return;
     }
 
-    if (req.query.providerid == false) {
+    if (req.query.providerid === false) {
         var errorMsg = 'User:'+userId+' didnot have blog!';
         log.debug(errorMsg);
         res.send(errorMsg);
@@ -266,7 +275,7 @@ router.get('/bot_bloglist', function (req, res) {
         return;
     }
 
-    User.findById(userId, function (err, user) {
+    UserDb.findById(userId, function (err, user) {
         var p;
         var apiUrl;
 
@@ -282,35 +291,37 @@ router.get('/bot_bloglist', function (req, res) {
                 return;
             }
 
-            //log.debug(body);
+            log.debug(body);
 
             var sendData = {};
             sendData.provider = p;
             sendData.blogs = [];
 
+            log.debug("pageId: "+p.providerId+", pageName: feed, pageLink: https://www.facebook.com");
+            sendData.blogs.push({"blog_id":p.providerId, "blog_title":"feed", "blog_url":"https://www.facebook.com"});
+
             for (var i = 0; i  < body.data.length; i+=1) {
                 var item = body.data[i];
-
                 var pageId = item.id;
                 var pageName = item.name;
-                var pageLink;
-
+                var pageLink = "https://www.facebook.com";
+                /*
                 apiUrl = FACEBOOK_API_URL+"/"+pageId;
                 log.debug(apiUrl);
-
                 _requestGet(apiUrl, p.accessToken, function(err, response, pageBody) {
                     var hasError = _checkError(err, response, pageBody);
                     if (hasError !== undefined) {
+                        log.debug("hasError: "+hasError);
                         res.send(hasError);
                         return;
                     }
                     pageLink = pageBody.link;
-                    log.debug(pageLink);
+                    //log.debug("pageId: "+pageId+", pageName: "+pageName+", pageLink: "+pageLink);
                 });
+                */
+                log.debug("pageId: "+pageId+", pageName: "+pageName+", pageLink: "+pageLink);
                 sendData.blogs.push({"blog_id":pageId, "blog_title":pageName, "blog_url":pageLink});
             }
-
-            log.debug(sendData);
             res.send(sendData);
         });
     });
