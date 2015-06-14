9570dac60b9176b6756591e798e77aeb83edc323
Alec Kim <kimalec7@gmail.com>
2015-06-14 17:52:46 +0900

    #309 log system
    set log level to info and make logfile on  development mode
    set log level to debug on production mode without logfile.
    
    app.js
    set task interval time to 1 min

--- controllers/log.js
+++ controllers/log.js
@@ -9,19 +9,34 @@ var winston = require('winston');
 //silly, debug, verbose, info, warn, error
 
 module.exports = function(filename) {
-    var logger = new winston.Logger({
-        transports: [
-            new winston.transports.Console({
-                level      : 'info',
-                colorize   : true
-            }),
-            new winston.transports.File({
-                level      : "debug",
-                json       : false,
-                filename   : filename
-            })
-        ]
-    });
+    var logger;
+    //debug mode
+    if (filename)  {
+        logger = new winston.Logger({
+            transports: [
+                new winston.transports.Console({
+                    level      : 'info',
+                    colorize   : true
+                }),
+                new winston.transports.File({
+                    level      : "debug",
+                    json       : false,
+                    filename   : filename
+                })
+            ]
+        });
+    }
+    else {
+        logger = new winston.Logger({
+            transports: [
+                new winston.transports.Console({
+                    level      : 'debug',
+                    colorize   : true
+                })
+            ]
+        });
+    }
+
     logger.exitOnError = false;
     return logger;
 };
